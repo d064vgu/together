1e515cec3821f19a4c544c890f7944cfbf8ee6cb
MA JACK <suzukikaixuan@gmail.com>
2023-05-19 14:30:42 +0900

    JIは不正なコマンドを削除して、ほかのコマンドを追加した

--- hello_world.c
+++ hello_world.c
@@ -3,6 +3,7 @@
 int main(){
     printf("hello world\n");
     printf("my hobby is reading\n");
-    printf("i am in branch2nd\n")
+    //printf("i am in branch2nd\n")
+    printf("this is JI KAIXUAN operating\n");
     return 0;
 }
