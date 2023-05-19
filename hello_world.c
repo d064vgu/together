009045c016d094c8822baf665045848e8e562394
unknown <d064vgu@yamaguchi-u.ac.jp>
2023-05-19 14:46:06 +0900

    自分の名前の出力と返信を追加しました

--- hello_world.c
+++ hello_world.c
@@ -5,5 +5,7 @@ int main(){
     printf("my hobby is reading\n");
     //printf("i am in branch2nd\n")
     printf("this is JI KAIXUAN operating\n");
+    printf("this is ZHANG TIANYI\n");
+    printf("sorry about that");
     return 0;
 }
